9e9f8621bbf0e7fa73d678879720dc8b9a88952c
LearnerAnuprash <info.anuprash@gmail.com>
2025-06-18 22:51:20 +0545

    intial commit

--- index1.js
+++ index1.js
@@ -3,5 +3,9 @@ const {add,sub}=require("./index2.js");
 const firstNumber=2;
 const secondNumber=3;
 
+
+
+
+
 console.log(`The sum of ${firstNumber} & ${secondNumber} is: ${add(firstNumber,secondNumber)}`);
 console.log(`The difference of ${firstNumber}& ${secondNumber} is: ${sub(firstNumber,secondNumber)}`);
